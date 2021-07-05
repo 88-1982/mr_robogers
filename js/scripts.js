5b0c2a849ad6b8c99c9fb1961da33ba4e01b5001
De Aunte Hall <deauntehall@Des-MacBook-Pro.local>
2021-07-05 15:56:20 -0700

    add UL and BL to js files

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,31 @@
 function numberMessage(result) {
   let number =$("input#number").val();
   let array = [];
-  for (let i=0; i <= i++); {
+  for (let i=0; i <= i++) {
     array.push(i.toString());
-
+    if (array[i].inculdes("3")) {
+      result.push ("Won't you be my neighbor?");
+    } else if (array[i].inculdes("2")) {
+      result.push("boop!");
+    } else if (array[i].inculdes("1")); {
+      result.push("Beep!");
+    }
+      else {
+        result.push(array[i]+",");
+      }
+    }
+    return result
   }
-}
+
+  //UI Logic
+  $(document).ready(function(){
+    $("form.form").submit(function(event){
+      event.preventDefult();
+      let number =$("input#number").val();
+
+      newResult = numberMessage(number);
+      $(".container2").show();
+      $("#output").empty();
+      $("#output").append(newResult);
+    });
+  });
